37c560e0dcce4b27c8b59cd9e42ca229095cfa2f
Shailja Maheshwari <mshailja29@gmail.com>
2024-04-26 23:56:06 -0400

    js client side validation for users register and login

--- routes/users.js
+++ routes/users.js
@@ -11,18 +11,36 @@ router.route('/').get(async(req,res) => {
 
 router.route('/register')
 .get(async(req,res)=>{
-    return res.render('users',{title:'User Register'})
+    return res.render('register',{title:'User Register'})
 })
 
 .post(async(req,res) => {
     try{
-        let {fname,lname,email,password,role} = req.body;
+        let {fname,lname,email,password,confirmPassword,role} = req.body;
 
-        if(!fname || !lname || !email || !password || !role) throw "All fields must be supplied";
+        // if(!fname || !lname || !email || !password || !role) throw "All fields must be supplied";
+        const fields = [
+            { value: fname, name: 'First name' },
+            { value: lname, name: 'Last name' },
+            { value: email, name: 'Email' },
+            { value: password, name: 'Password' },
+            {value: confirmPassword, name:'Confirm Password'},
+            { value: role, name: 'Role'}
+        ];
+        
+        for (const field of fields) {
+            if (!field.value) {
+                throw (field.name + ' cannot be empty');
+            }
+        }
     
         ({fname,lname,email,password,role} = validation.checkUser(req.body));
+        if(confirmPassword!==password)
+        {
+            return res.status(400).render('register',{error:'Password does not match'})
+        }
     
-        let result = await registerUser(fname,lname,email,password,role);
+        let result =    await registerUser(fname,lname,email,password,role);
 
         if(!result) return res.status(500).send({error:'Internal Server Error'});
 
@@ -50,8 +68,34 @@ router.route('/login')
     return res.render('login',{title:"login"})
  })
 .post(async(req,res)=>{
+    let {email,password} = req.body
+   
+    try{
+        if(!email || !password){
+            return res.status(400).render('login',{error:true,msg:'Both email and password are required.'})
+        }
     
-})
+      
+            email = email.toLowerCase()
+        email = validation.checkString(email,'Email')
+        // validation.checkEmail(email)
+        
+            password = validation.checkString(password,'password')
+            validation.checkPassword(password,'Password')
+       
+        
+        let result = await loginUser(email,password)
+        if(!result) return res.status(500).send({error:'Internal Server Error'});
+        req.session.user = result;
+        return res.redirect('/dashboard');
+    }
+        catch(e)
+        {
+            console.log(e)
+        }
+
+    
+}) 
 
 router.route('/dashboard')
 .get(async (req, res) => {
